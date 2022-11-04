3defae239630e75646e4e145ecfa1d945cc728ae
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2022-11-04 13:21:11 +0100

    secrets from env

--- lib/zometv_com_web/endpoint.ex
+++ lib/zometv_com_web/endpoint.ex
@@ -1,6 +1,28 @@
 defmodule ZometvComWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :zometv_com
 
+  def init(_key, config) do
+    if config[:load_from_system_env] do
+      port =
+       System.get_env("PORT") ||
+       raise("expected the PORT environment variable to be set")
+
+      secret_key_base =
+        System.get_env("SECRET_KEY_BASE") ||
+          raise("expected the SECRET_KEY_BASE environment variable to be set")
+
+      config =
+        config
+        |> Keyword.put(:http, [:inet6, port: port])
+        |> Keyword.put(:secret_key_base, secret_key_base)
+        |> Keyword.put(:url, [host: "zometv.com", port: port])
+
+      {:ok, config}
+    else
+      {:ok, config}
+    end
+  end
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
